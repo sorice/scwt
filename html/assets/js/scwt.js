2fd868bd4dc91ee1f0ad980ff7ba6cef518b076a
Leonel Salazar Videaux <leonel.lordford@gmail.com>
2016-09-19 17:44:01 -0400

    - Actualizando index.html con los elementos de la revisión de Abel. - Adicionando animate.css, wow.js y los ficheros CSS/JS propios   del proyecto SCWT.

---
+++ html/assets/js/scwt.js
@@ -0,0 +1,14 @@
+//SmothScroll
+$('a[href*=#]').click(function() {
+    if (this.hash == "#carousel1") {return true;}
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+    && location.hostname == this.hostname) {
+        var $target = $(this.hash);
+        $target = $target.length && $target || $('[name=' + this.hash.slice(1) +']');
+        if ($target.length) {
+                var targetOffset = $target.offset().top;
+                $('html,body').animate({scrollTop: targetOffset}, 1000);
+                return false;
+        }
+    }
+});
